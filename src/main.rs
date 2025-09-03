eb93469bf846d4d68d082a4824a1525e45767132
Alan <avaldevenito@fi.uba.ar>
2025-09-03 00:55:27 -0300

    add repl() loop with stdin reading

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
+use std::io::{self, Write};
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() == 1 {
-        // TODO: Implement REPL
+        repl();
     } else if args.len() == 2 {
         // TODO: Implement file reading and execution
     } else {
         eprintln!("Usage: rlox [path]");
         std::process::exit(64);
+    }
+}
+
+fn repl() {
+    let stdin = io::stdin();
+
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut line = String::new();
+        
+        if stdin.read_line(&mut line).unwrap() == 0 {
+            println!();
+            break;
+        }
+
+        // TODO: Implement interpret function
     }
 }

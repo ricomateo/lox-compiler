8858b43c08e5c72bb8ca90f96086b390a4cff164
Alan <avaldevenito@fi.uba.ar>
2025-09-03 01:31:06 -0300

    add scanner struct

---
+++ src/scanner.rs
@@ -0,0 +1,17 @@
+pub struct Scanner {
+    source: Vec<char>,
+    start: usize,
+    current: usize,
+    line: usize,
+}
+
+impl Scanner {
+    pub fn new(source: &str) -> Self {
+        Self {
+            source: source.chars().collect(),
+            start: 0,
+            current: 0,
+            line: 1,
+        }
+    }
+}

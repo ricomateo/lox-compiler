cd45bc0ff82130232edf26f5de519c61dad6359a
ricomateo <mrico@fi.uba.ar>
2025-09-05 20:54:27 -0300

    try nested operations

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,16 @@ use lox_compiler::{
 
 fn main() {
     let mut chunk = Chunk::new();
-    let constant_index = chunk.add_constant(Value::Number(1.2));
     let line = 123;
+    let constant_index = chunk.add_constant(Value::Number(1.2));
     chunk.write(OpCode::Constant(constant_index), line);
+    let constant_index = chunk.add_constant(Value::Number(3.4));
+    chunk.write(OpCode::Constant(constant_index), line);
+    chunk.write(OpCode::Add, line);
+    let constant_index = chunk.add_constant(Value::Number(5.6));
+    chunk.write(OpCode::Constant(constant_index), line);
+    chunk.write(OpCode::Divide, line);
+
     chunk.write(OpCode::Negate, line);
     chunk.write(OpCode::Return, line);
 

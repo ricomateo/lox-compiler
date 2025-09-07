2d1c48ec3db70c70f3c40906ace1d407918c4efe
Alan <avaldevenito@fi.uba.ar>
2025-09-07 19:33:19 -0300

    add literal token handling

--- src/scanner.rs
+++ src/scanner.rs
@@ -86,6 +86,10 @@ impl<'a> Scanner<'a> {
 
         let c = self.advance();
 
+        if Self::is_digit(c) {
+            return self.number();
+        }
+
         match c {
             // Single-character tokens
             '(' => self.make_token(TokenType::LeftParen),
@@ -128,6 +132,8 @@ impl<'a> Scanner<'a> {
                     self.make_token(TokenType::Greater)
                 }
             }
+            // Literals
+            '"' => self.string(),
             _ => self.error_token("Unexpected character"),
         }
     }
@@ -209,5 +215,41 @@ impl<'a> Scanner<'a> {
         let mut iter = self.source[self.current..].chars();
         iter.next();
         iter.next().unwrap_or('\0')
+    }
+
+    fn string(&mut self) -> Token {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            return self.error_token("Unterminated string");
+        }
+
+        self.advance();
+        self.make_token(TokenType::String)
+    }
+
+    fn is_digit(c: char) -> bool {
+        c >= '0' && c <= '9'
+    }
+
+    fn number(&mut self) -> Token {
+        while Self::is_digit(self.peek()) {
+            self.advance();
+        }
+
+        if self.peek() == '.' && Self::is_digit(self.peek_next()) {
+            self.advance();
+
+            while Self::is_digit(self.peek()) {
+                self.advance();
+            }
+        }
+
+        self.make_token(TokenType::Number)
     }
 }

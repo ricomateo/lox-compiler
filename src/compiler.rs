cfbdcb24c02b4582c28892d2d0ca9bac848ab7f9
Alan <avaldevenito@fi.uba.ar>
2025-09-07 18:45:48 -0300

    add Token struct, TokenType enum and implement compile()

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,3 +1,24 @@
-pub fn compile(_source: &str) {
-    // TODO: Implement scanner
+use crate::scanner::{Scanner, TokenType};
+
+pub fn compile(source: &str) {
+    let mut scanner = Scanner::new(source);
+
+    let mut line: isize = -1;
+
+    loop {
+        let token = scanner.scan_token();
+
+        if token.line as isize != line {
+            print!("{:4} ", token.line);
+            line = token.line as isize;
+        } else {
+            print!("   | ");
+        }
+
+        println!("{:2?} '{}'", token.kind as u8, &scanner.source[token.start..token.start + token.lenght]);
+
+        if token.kind == TokenType::Eof {
+            break;
+        }
+    }
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -1,17 +1,86 @@
-pub struct Scanner {
-    source: Vec<char>,
+pub struct Scanner<'a> {
+    pub source: &'a str,
     start: usize,
     current: usize,
     line: usize,
 }
 
-impl Scanner {
-    pub fn new(source: &str) -> Self {
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum TokenType {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier,
+    String,
+    Number,
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Error,
+    Eof,
+}
+
+#[derive(Debug)]
+pub struct Token {
+    pub kind: TokenType,
+    pub start: usize,
+    pub lenght: usize,
+    pub line: usize,
+}
+
+impl<'a> Scanner<'a> {
+    pub fn new(source: &'a str) -> Self {
         Self {
-            source: source.chars().collect(),
+            source,
             start: 0,
             current: 0,
             line: 1,
+        }
+    }
+
+    pub fn scan_token(&mut self) -> Token {
+        Token {
+            kind: TokenType::Eof,
+            start: 0,
+            lenght: 0,
+            line: self.line,
         }
     }
 }

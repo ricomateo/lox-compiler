aadf7c0a5d47cb878eb6e549e7858c4b372d8066
ricomateo <mrico@fi.uba.ar>
2025-09-10 00:09:37 -0300

    add parser

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,28 +1,85 @@
-use crate::scanner::{Scanner, TokenType};
+use crate::{
+    chunk::Chunk,
+    scanner::{Scanner, Token, TokenType},
+};
 
-pub fn compile(source: &str) {
-    let mut scanner = Scanner::new(source);
+pub fn compile(source: String) -> Result<(), &'static str> {
+    let mut parser = Parser::new(source);
+    parser.advance();
+    // expression(); TODO
+    parser.consume(TokenType::Eof, "Expected end of expression");
+    if parser.had_error {
+        return Err("");
+    }
+    Ok(())
+}
 
-    let mut line: isize = -1;
+struct Parser {
+    current: Option<Token>,
+    previous: Option<Token>,
+    had_error: bool,
+    panic_mode: bool,
+    // TODO: consider removing scanner from here
+    scanner: Scanner,
+}
 
-    loop {
-        let token = scanner.scan_token();
-
-        if token.line as isize != line {
-            print!("{:4} ", token.line);
-            line = token.line as isize;
-        } else {
-            print!("   | ");
+impl Parser {
+    fn new(source: String) -> Self {
+        Self {
+            current: None,
+            previous: None,
+            had_error: false,
+            panic_mode: false,
+            scanner: Scanner::new(source),
         }
+    }
 
-        println!(
-            "{:2?} '{}'",
-            token.kind as u8,
-            &scanner.source[token.start..token.start + token.length]
-        );
-
-        if token.kind == TokenType::Eof {
-            break;
+    fn advance(&mut self) {
+        loop {
+            let token = self.scanner.scan_token();
+            if token.kind != TokenType::Error {
+                break;
+            }
+            self.current = Some(token);
+            self.error_at_current("");
         }
+    }
+
+    fn consume(&mut self, token_type: TokenType, message: &str) {
+        if let Some(token) = &self.current {
+            if token.kind == token_type {
+                self.advance();
+                return;
+            }
+        }
+        self.error_at_current(message);
+    }
+
+    fn error_at_current(&mut self, message: &str) {
+        let token = &self.current.clone().unwrap();
+        self.error_at(token, message);
+    }
+
+    fn error_at(&mut self, token: &Token, message: &str) {
+        if self.panic_mode {
+            return;
+        }
+        self.panic_mode = true;
+        eprint!("[line {}] Error", token.line);
+
+        match token.kind {
+            TokenType::Eof => {
+                eprint!(" at end");
+            }
+            // Do nothing
+            TokenType::Error => (),
+            _ => {
+                // TODO: check if it is correct to print the lexeme here
+                // https://craftinginterpreters.com/compiling-expressions.html#handling-syntax-errors
+                eprint!(" at '{}'", token.lexeme);
+            }
+        }
+        eprintln!(": {message}");
+        self.had_error = true;
     }
 }

788a5f51e113b1e5ae707d10c8fc88afc05d1058
Alan <avaldevenito@fi.uba.ar>
2025-09-23 17:07:36 -0300

    add helper functions for compiler

--- src/compiler.rs
+++ src/compiler.rs
@@ -4,6 +4,7 @@ use crate::{
 };
 
 use crate::expr::Literal;
+use crate::scanner::Token;
 use crate::scanner::TokenType;
 
 pub struct Compiler {
@@ -30,70 +31,14 @@ impl Compiler {
                 operator,
                 right,
             } => {
-                self.compile_expr(left);
-                self.compile_expr(right);
-
-                match operator.kind {
-                    TokenType::Plus => {
-                        self.emit_byte(OpCode::Add, operator.line);
-                    }
-                    TokenType::Minus => {
-                        self.emit_byte(OpCode::Subtract, operator.line);
-                    }
-                    TokenType::Star => {
-                        self.emit_byte(OpCode::Multiply, operator.line);
-                    }
-                    TokenType::Slash => {
-                        self.emit_byte(OpCode::Divide, operator.line);
-                    }
-                    TokenType::EqualEqual => {
-                        self.emit_byte(OpCode::Equal, operator.line);
-                    }
-                    TokenType::BangEqual => {
-                        // Syntactic sugar: a != b  is the same as !(a == b)
-                        self.emit_byte(OpCode::Equal, operator.line);
-                        self.emit_byte(OpCode::Not, operator.line);
-                    }
-                    TokenType::Greater => {
-                        self.emit_byte(OpCode::Greater, operator.line);
-                    }
-                    TokenType::GreaterEqual => {
-                        // Syntactic sugar: a >= b  is the same as !(a < b)
-                        self.emit_byte(OpCode::Less, operator.line);
-                        self.emit_byte(OpCode::Not, operator.line);
-                    }
-                    TokenType::Less => {
-                        self.emit_byte(OpCode::Less, operator.line);
-                    }
-                    TokenType::LessEqual => {
-                        // Syntactic sugar: a <= b  is the same as !(a > b)
-                        self.emit_byte(OpCode::Greater, operator.line);
-                        self.emit_byte(OpCode::Not, operator.line);
-                    }
-                    _ => unreachable!(),
-                }
+                self.compile_binary(operator, left, right);
             }
             Expr::Unary { operator, right } => {
-                self.compile_expr(right);
-
-                match operator.kind {
-                    TokenType::Minus => self.emit_byte(OpCode::Negate, operator.line),
-                    TokenType::Bang => self.emit_byte(OpCode::Not, operator.line),
-                    _ => unreachable!(),
-                }
+                self.compile_unary(operator, right);
             }
-            Expr::Literal(literal) => match literal {
-                Literal::Number(value) => {
-                    self.emit_constant(Value::Number(*value), 0);
-                }
-                Literal::Bool(value) => match value {
-                    true => self.emit_byte(OpCode::True, 0),
-                    false => self.emit_byte(OpCode::False, 0),
-                },
-                Literal::Nil => {
-                    self.emit_byte(OpCode::Nil, 0);
-                }
-            },
+            Expr::Literal(literal) => {
+                self.compile_literal(literal);
+            }
             Expr::Grouping { expression } => {
                 self.compile_expr(expression);
             }
@@ -117,5 +62,67 @@ impl Compiler {
 
     fn end_compiler(&mut self) {
         self.emit_byte(OpCode::Return, 0);
+    }
+
+    fn compile_binary(&mut self, operator: &Token, left: &Expr, right: &Expr) {
+        self.compile_expr(left);
+        self.compile_expr(right);
+
+        match operator.kind {
+            TokenType::Plus => self.emit_byte(OpCode::Add, operator.line),
+            TokenType::Minus => self.emit_byte(OpCode::Subtract, operator.line),
+            TokenType::Star => self.emit_byte(OpCode::Multiply, operator.line),
+            TokenType::Slash => self.emit_byte(OpCode::Divide, operator.line),
+            TokenType::EqualEqual => {
+                self.emit_byte(OpCode::Equal, operator.line);
+            }
+            TokenType::BangEqual => {
+                // Syntactic sugar: a != b  is the same as !(a == b)
+                self.emit_byte(OpCode::Equal, operator.line);
+                self.emit_byte(OpCode::Not, operator.line);
+            }
+            TokenType::Greater => {
+                self.emit_byte(OpCode::Greater, operator.line);
+            }
+            TokenType::GreaterEqual => {
+                // Syntactic sugar: a >= b  is the same as !(a < b)
+                self.emit_byte(OpCode::Less, operator.line);
+                self.emit_byte(OpCode::Not, operator.line);
+            }
+            TokenType::Less => {
+                self.emit_byte(OpCode::Less, operator.line);
+            }
+            TokenType::LessEqual => {
+                // Syntactic sugar: a <= b  is the same as !(a > b)
+                self.emit_byte(OpCode::Greater, operator.line);
+                self.emit_byte(OpCode::Not, operator.line);
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    fn compile_unary(&mut self, operator: &Token, right: &Expr) {
+        self.compile_expr(right);
+
+        match operator.kind {
+            TokenType::Minus => self.emit_byte(OpCode::Negate, operator.line),
+            TokenType::Bang => self.emit_byte(OpCode::Not, operator.line),
+            _ => unreachable!(),
+        }
+    }
+
+    fn compile_literal(&mut self, literal: &Literal) {
+        match literal {
+            Literal::Number(value) => {
+                self.emit_constant(Value::Number(*value), 0);
+            }
+            Literal::Bool(value) => match value {
+                true => self.emit_byte(OpCode::True, 0),
+                false => self.emit_byte(OpCode::False, 0),
+            },
+            Literal::Nil => {
+                self.emit_byte(OpCode::Nil, 0);
+            }
+        }
     }
 }
